0f6c1a2f007d84f5bd8a5ce050c24905acb9d6ae
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-02-24 09:18:14 +0800

    added auth login with google

--- src/components/molecular/ListJob.js
+++ src/components/molecular/ListJob.js
@@ -1,94 +1,107 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { postedAt } from '../../utils';
 
-function ListJob() {
-  const [data, setData] = useState([]);
-  const getData = () => {
-    fetch(
-      'positions.json',
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      },
-    )
-      .then((response) => response.json())
-      .then((myJson) => {
-        setData(myJson);
-      });
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
+export default function ListJob(props) {
+  const {
+    job,
+    isLoading,
+  } = props;
   return (
     <div>
       {
-        data.map((jobList) => (
-          <div key={jobList?.id}>
-            <div className="w-full mt-5 flex gap-2 border-b-[0.1px] border-pink-100">
-              <div className="flex-none">
-                <div className="max-w-[4rem] max-h-[4rem] rounded-full mx-auto">
-                  <img src={jobList?.company_logo} alt={jobList?.company_logo} />
-                </div>
-              </div>
-              <div className="flex-initial w-full">
-                <div>
-                  <Link to={`/detail/${jobList?.id}`}>
-                    <h3 className="text-pink-400 text-base font-semibold hover:underline mt-2">
-                      {
-                        jobList?.title
-                      }
-                    </h3>
-                  </Link>
-                </div>
-                <div>
-                  <Link to={jobList?.company_url}>
-                    <p className="text-xs mt-1 text-slate-900 hover:underline">
-                      {
-                        jobList?.company
-                      }
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <div className="w-full flex gap-2">
-                    <div className="flex-none">
-                      <p className="text-xs mt-1 text-slate-400">
-                        {
-                          jobList?.location
-                        }
-                      </p>
-                    </div>
-                    <p className="text-xs mt-1 text-slate-400">
-                      { ' - ' }
-                    </p>
-                    <div className="w-full flex-initial">
-                      <p className="text-xs mt-1 text-slate-400">
-                        {
-                          jobList?.type
-                        }
-                      </p>
-                    </div>
+        isLoading ? (
+          <p>loading</p>
+        ) : (
+          job.map((jobList) => (
+            <div key={jobList?.id}>
+              <div className="w-full mt-5 flex gap-2 border-b-[0.1px] border-pink-100">
+                <div className="flex-none">
+                  <div className="max-w-[4rem] max-h-[4rem] rounded-full mx-auto">
+                    <img src={jobList?.company_logo} alt={jobList?.company_logo} />
                   </div>
                 </div>
-                <div>
-                  <p className="text-xs mt-2 text-green-800 font-medium">
-                    {
-                      postedAt(jobList?.created_at)
-                    }
-                  </p>
+                <div className="flex-initial w-full">
+                  <div>
+                    <Link to={`/detail/${jobList?.id}`}>
+                      <h3 className="text-pink-400 text-base font-semibold hover:underline mt-2">
+                        {
+                          jobList?.title
+                        }
+                      </h3>
+                    </Link>
+                  </div>
+                  <div>
+                    <Link to={jobList?.company_url}>
+                      <p className="text-xs mt-1 text-slate-900 hover:underline">
+                        {
+                          jobList?.company
+                        }
+                      </p>
+                    </Link>
+                  </div>
+                  <div>
+                    <div className="w-full flex gap-2">
+                      <div className="flex-none">
+                        <p className="text-xs mt-1 text-slate-400">
+                          {
+                            jobList?.location
+                          }
+                        </p>
+                      </div>
+                      <p className="text-xs mt-1 text-slate-400">
+                        { ' - ' }
+                      </p>
+                      <div className="w-full flex-initial">
+                        <p className="text-xs mt-1 text-slate-400">
+                          {
+                            jobList?.type
+                          }
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="text-xs mt-2 text-green-800 font-medium">
+                      {
+                        postedAt(jobList?.created_at)
+                      }
+                    </p>
+                  </div>
+                  <br />
                 </div>
-                <br />
               </div>
             </div>
-          </div>
-        ))
+          ))
+        )
       }
     </div>
   );
 }
 
-export default ListJob;
+const jobShape = {
+  title: PropTypes.string,
+  company_url: PropTypes.string,
+  company: PropTypes.string,
+  location: PropTypes.string,
+  type: PropTypes.bool,
+  created_at: PropTypes.string,
+};
+
+ListJob.propTypes = {
+  job: PropTypes.objectOf(PropTypes.shape(jobShape)),
+  isLoading: PropTypes.bool,
+};
+
+ListJob.defaultProps = {
+  isLoading: false,
+  job: {
+    title: '',
+    company_url: '',
+    company: '',
+    location: '',
+    type: true,
+    created_at: '',
+  },
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,52 @@
-import React from 'react';
+/* eslint-disable no-shadow */
+/* eslint-disable consistent-return */
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import SearchCheckbox from '../components/atoms/SearchCheckbox';
 import SearchText from '../components/atoms/SearchText';
 import ListJob from '../components/molecular/ListJob';
+import useInput from '../hooks/useInput';
 
 function HomePage() {
+  const [data, setData] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [search, setSearch] = useInput(() => searchParams.get('search') || '');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const onSearchChangeHandler = (search) => {
+    setSearch(search);
+    setSearchParams({ search });
+  };
+
+  const getData = () => {
+    fetch(
+      'positions.json',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then((myJson) => {
+        const myData = myJson.filter((item) => {
+          if (item.title.toString().toLowerCase().includes(search.toLowerCase())) return true;
+          if (item.location.toString().toLowerCase().includes(search.toLowerCase())) return true;
+        });
+        setIsLoading(false);
+        setData(myData);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [getData()]);
   return (
     <div className="w-full justify-center p-10">
       <div className="flex mx-auto gap-4">
         <div className="flex-none">
-          <SearchText />
+          <SearchText search={search} searchChange={onSearchChangeHandler} />
         </div>
         <div className="flex-none mt-[17px] text-xs">
           <div className="flex gap-4">
@@ -22,7 +60,7 @@ function HomePage() {
         </div>
       </div>
       <div className="mt-8">
-        <ListJob />
+        <ListJob job={data} isLoading={isLoading} />
       </div>
     </div>
   );
