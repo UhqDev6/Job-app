994479485afd4772060509208338b7db4e31d7ec
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-02-26 15:57:10 +0800

    update homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
+// import jwtDecode from 'jwt-decode';
 import Header from './components/molecular/Header';
 import { DETAIL_PAGE, HOME_PAGE } from './constants/path';
 import DetailPage from './pages/DetailPage';
@@ -8,73 +8,88 @@ import HomePage from './pages/HomePage';
 import './style/App.css';
 
 export default function App() {
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
 
-  const handleCallbackResponse = (response) => {
-    // console.log(`encoded JWT ${response.credential}`);
-    const userObject = jwtDecode(response.credential);
-    // console.log(userObject);
-    setUser(userObject);
-    document.getElementById('signInDiv').hidden = true;
-  };
+  // const handleCallbackResponse = (response) => {
+  //   // console.log(`encoded JWT ${response.credential}`);
+  //   const userObject = jwtDecode(response.credential);
+  //   // console.log(userObject);
+  //   setUser(userObject);
+  //   document.getElementById('signInDiv').hidden = true;
+  // };
 
-  const handleSignOut = (event) => {
-    setUser({});
-    document.getElementById('signInDiv').hidden = false;
-  };
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: '467734937742-ebd3kr8s7824ma80bv7448ku9hpimk34.apps.googleusercontent.com',
-      callback: handleCallbackResponse,
-    });
+  // const handleSignOut = (event) => {
+  //   setUser({});
+  //   document.getElementById('signInDiv').hidden = false;
+  // };
+  // useEffect(() => {
+  //   /* global google */
+  //   google.accounts.id.initialize({
+  //     client_id: '467734937742-ebd3kr8s7824ma80bv7448ku9hpimk34.apps.googleusercontent.com',
+  //     callback: handleCallbackResponse,
+  //   });
 
-    google.accounts.id.renderButton(
-      document.getElementById('signInDiv'),
-      { theme: 'outline', size: 'large' },
-    );
+  //   google.accounts.id.renderButton(
+  //     document.getElementById('signInDiv'),
+  //     { theme: 'outline', size: 'large' },
+  //   );
 
-    google.accounts.id.prompt();
-  }, []);
+  //   google.accounts.id.prompt();
+  // }, []);
+
+  // return (
+  //   <div>
+  //     <div>
+  //       <div id="signInDiv" />
+  //       {
+  //         Object.keys(user).length !== 0
+  //         && (
+  //           <div>
+  //             <div>
+  //               <header>
+  //                 <Header />
+  // <img className="max-w-[4rem] max-h-[4rem] rounded-full
+  // mx-auto" src={user?.picture} alt={user?.picture} />
+  //                 <p className="text-center text-sm text-pink-500">{`Welcome ${user?.name}`}</p>
+  //                 <button
+  //                   type="button"
+  //                   onClick={(e) => handleSignOut(e)}
+  //                   className="text-pink-500 text-xs flex mx-auto mt"
+  //                 >
+  //                   Sign Out
+  //                 </button>
+  //               </header>
+  //               <main>
+  //                 <Routes>
+  //                   <Route path={HOME_PAGE} element={<HomePage />} />
+  //                   <Route path={DETAIL_PAGE} element={<DetailPage />} />
+  //                 </Routes>
+  //               </main>
+  //             </div>
+  //           </div>
+  //         )
+  //       }
+  //       {
+  //         user
+  //         && (
+  //           <div />
+  //         )
+  //       }
+  //     </div>
+  //   </div>
+  // );
 
   return (
     <div>
-      <div>
-        <div id="signInDiv" />
-        {
-          Object.keys(user).length !== 0
-          && (
-            <div>
-              <div>
-                <header>
-                  <Header />
-                  <img className="max-w-[4rem] max-h-[4rem] rounded-full mx-auto" src={user?.picture} alt={user?.picture} />
-                  <p className="text-center text-sm text-pink-500">{`Welcome ${user?.name}`}</p>
-                  <button
-                    type="button"
-                    onClick={(e) => handleSignOut(e)}
-                    className="text-pink-500 text-xs flex mx-auto mt"
-                  >
-                    Sign Out
-                  </button>
-                </header>
-                <main>
-                  <Routes>
-                    <Route path={HOME_PAGE} element={<HomePage />} />
-                    <Route path={DETAIL_PAGE} element={<DetailPage />} />
-                  </Routes>
-                </main>
-              </div>
-            </div>
-          )
-        }
-        {
-          user
-          && (
-            <div />
-          )
-        }
-      </div>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path={HOME_PAGE} element={<HomePage />} />
+          <Route path={DETAIL_PAGE} element={<DetailPage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
